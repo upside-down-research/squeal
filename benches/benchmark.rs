7d072a8b8749a40745d33517bd942c98ff015624
Paul Nathan <paul@nathan.house>
2023-12-31 12:56:18 -0800

    chore: Added simple benchmark framework.
    * Using criterion as a benchmark system.
    * Not vendoring dependencies.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,15 +1,8 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use squeal::*;
 
-#[test]
-fn test_select() {
-    let result = Select::new(Columns::Star).sql();
-    assert_eq!(result, "SELECT *");
-}
-
-// Integration tests exercizing the complicated functionality of the squeal library for the Query object
-#[test]
-fn test_complicated_query_builder() {
-    let result = Query {
+fn generate() -> String {
+        let result = Query {
         select: Select::new(Columns::Selected(vec!["a".to_string(), "b".to_string()])),
         from: "table".to_string(),
         where_clause: Some(
@@ -31,6 +24,13 @@ fn test_complicated_query_builder() {
                                               OrderedColumn::Desc("b".to_string())]}),
         limit: Some(19),
         offset: Some(10),
-    }.sql();
-    assert_eq!(result, "SELECT a, b FROM table WHERE a <> b GROUP BY a, b HAVING a <> b ORDER BY a ASC, b DESC LIMIT 19 OFFSET 10");
+        }.sql();
+    result
 }
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("generate", |b| b.iter(|| { generate(); } ));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
